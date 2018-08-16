0426e307c3372573398b79f478d972e9613aed4f
quang <six3up@gmail.com>
2018-08-16 15:17:38 +0700

    update last

--- slide/slide.js
+++ slide/slide.js
@@ -4,7 +4,7 @@
  */
 
 var sliderImages = document.getElementsByClassName("slide"); // Pick first image.
-var current = 0;
+var current = 0; // First image (current: 0 -> 4)
 var temporal = document.getElementsByClassName("picture-name"); // Save the selected image.
 var dots = document.getElementsByClassName("dot-picture"); // Identify large image in small image.
 
